f9ab80939f4f08f91dcc793032eb2ed9d5d73acb
Quintin Henn <quintin.henn@gmail.com>
2019-01-18 13:05:39 +0200

    Prettier fixes
    Prettier fixes for Travis CI to pass the build

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,17 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
 
-describe('generator-game-generic:app', () => {
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
+
+describe("generator-game-generic:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
+      .run(path.join(__dirname, "../generators/app"))
       .withPrompts({ appname: true });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("creates files", () => {
+    assert.file(["README.md"]);
   });
 });
