f9b1e83eb850f5842e2db10589f889c8956f7ba2
Quintin Henn <quintin.henn@gmail.com>
2020-08-08 13:29:44 +0200

    Fix Jest Yeoman generator test
    Update dev dependencies, update template folder structure, update test file

---
+++ __tests__/test.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
+
+describe("generator game: prompting for appname", () => {
+  const componentName = "dummy-game";
+  let tempPath;
+
+  beforeAll((done) => {
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ appname: componentName })
+      .on("end", done)
+      .then((result) => {
+        tempPath = result;
+      });
+  });
+
+  it("created base project files", () => {
+    assert.file([
+      `${tempPath}/${componentName}/README.md`,
+      `${tempPath}/${componentName}/LICENSE.md`
+    ]);
+  });
+
+  it("created base dot files", () => {
+    assert.file([
+      `${tempPath}/${componentName}/.editorconfig`,
+      `${tempPath}/${componentName}/.gitignore`,
+      `${tempPath}/${componentName}/.gitattributes`
+    ]);
+  });
+
+  it("created folder structure", () => {
+    assert.file([
+      `${tempPath}/${componentName}/assets/README.md`,
+      `${tempPath}/${componentName}/docs/README.md`,
+      `${tempPath}/${componentName}/game/Cordova/README.md`,
+      `${tempPath}/${componentName}/game/HTML5-Website/README.md`,
+      `${tempPath}/${componentName}/game/NWjs/README.md`,
+      `${tempPath}/${componentName}/game/Scirra-Arcade/README.md`,
+      `${tempPath}/${componentName}/source/README.md`
+    ]);
+  });
+});
+
+describe("generator game: give appname through arguments", () => {
+  const componentName = "dummy-game";
+  let tempPath;
+
+  beforeAll((done) => {
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withArguments(componentName)
+      .on("end", done)
+      .then((result) => {
+        tempPath = result;
+      });
+  });
+
+  it("created base project files", () => {
+    assert.file([`${tempPath}/${componentName}/README.md`]);
+  });
+
+  it("created base dot files", () => {
+    assert.file([
+      `${tempPath}/${componentName}/.editorconfig`,
+      `${tempPath}/${componentName}/.gitignore`,
+      `${tempPath}/${componentName}/.gitattributes`
+    ]);
+  });
+
+  it("created folder structure", () => {
+    assert.file([
+      `${tempPath}/${componentName}/assets/README.md`,
+      `${tempPath}/${componentName}/docs/README.md`,
+      `${tempPath}/${componentName}/game/Cordova/README.md`,
+      `${tempPath}/${componentName}/game/HTML5-Website/README.md`,
+      `${tempPath}/${componentName}/game/NWjs/README.md`,
+      `${tempPath}/${componentName}/game/Scirra-Arcade/README.md`,
+      `${tempPath}/${componentName}/source/README.md`
+    ]);
+  });
+});
